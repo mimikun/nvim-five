480a70bcd889f6de219599ad89c3129290f197cd
Yuto Tanaka <karaage_nennen@icloud.com>
2024-11-12 23:20:20 +0900

    add

---
+++ lua/plugins/iisca-nvim.lua
@@ -0,0 +1,18 @@
+--- enabled check
+---@return boolean
+local enabled = function()
+    local has_tiv = (1 == vim.fn.executable("tiv")) and true or false
+    return has_tiv
+end
+
+---@type LazySpec
+local spec = {
+    "Abizrh/iisca.nvim",
+    --lazy = false,
+    cmd = "ShowIisca",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    cond = enabled,
+    enabled = enabled,
+}
+
+return spec
